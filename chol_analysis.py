42765f3f50dcd68a66350c189d05f96b6acda8a1
Aidan <aar40@duke.edu>
2020-01-24 12:53:44 -0500

    i added the LDL function as a whole

--- chol_analysis.py
+++ chol_analysis.py
@@ -6,6 +6,16 @@ def HDL_analysis(HDL_level):
     else:
         return "Low"
 
+def LDL_analysis(LDL_leve):
+    if LDL_level >=190:
+        return "Very High"
+    elif 160 <= LDL_level < 190:
+        return "High"
+    elif 130 <= LDL_level < 160:
+        return "Borderline High"
+    elif LDL_level < 130:
+        return "Normal"
+
 
 def cholesterol_analysis():
     print("Cholesterol Analysis")
@@ -14,15 +24,18 @@ def cholesterol_analysis():
     if test_info[0] == "HDL":
         answer = HDL_analysis(int(test_info[1]))
         print("the Level is {} ".format(answer))
-        JDL_analysis()
+    if test_info[0] == "LDL":
+        answer = LDL_analysis(int(test_info[1]))
+        print("the Level is {} ".format(answer))
+
 
 
 def interface():
-    choice = 0
     while True:
-        print("Cholesterol Calc")
-        print("options")
-        print("   0- Quit")
+        print("Cholesterol Calculator")
+        print("Options:  ")
+        print("  1 - Cholesterol Analysis")
+        print("  9 - Quit")
         choice = input("Enter your option:  ")
         if choice == '9':
             return
