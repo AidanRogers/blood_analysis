2350719e55f1ade9904776dbc663aaecb9366d3b
Aidan <aar40@duke.edu>
2020-01-24 12:16:35 -0500

    interace loops until a good option is picked

---
+++ chol_analysis.py
@@ -0,0 +1,12 @@
+def interface():
+    choice = 0
+    while True:
+        print ("Cholesterol Calc")
+        print("options")
+        print("   0- Quit")
+        choice = input("Enter your option:  ")
+        if choice =='9':
+            return
+
+if __name__ == "__main__":
+    interface()
