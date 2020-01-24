377f7747ce21a8c2aa564be06b0144d72de963b8
Aidan <aar40@duke.edu>
2020-01-24 12:43:36 -0500

    i improved the cholesterol analysis by actualy creating the function, and had o commit early on the master branch because my terminal crashed and had to commit this change before switching back to my HDL branch

--- chol_analysis.py
+++ chol_analysis.py
@@ -1,22 +1,32 @@
+def HDL_analysis(HDL_level):
+        if HDL_level >= 60:
+            return "Normal"
+        elif 40<= HDL_level <60:
+            return "Borderline Low"
+        else:
+            return "Low"
+
 def cholesterol_analysis():
         print("Cholesterol Analysis")
-        HDLinput = inpit ("Enter test result: ")
+        HDLinput = input("Enter test result: ")
         test_info = HDLinput.split("=") #splits along the equal sign
         if test_info[0] == "HDL":
+            answer = HDL_analysis(int(test_info[1]))
+            print("the Level is {} ".format(answer))
             JDL_analysis()
 
 
 def interface():
     choice = 0
     while True:
-        print ("Cholesterol Calc")
+        print("Cholesterol Calc")
         print("options")
         print("   0- Quit")
         choice = input("Enter your option:  ")
-        if choice =='9':
+        if choice == '9':
             return
         elif choice == "1":
-                cholesterol_analysis()
+            cholesterol_analysis()
 
 if __name__ == "__main__":
     interface()
